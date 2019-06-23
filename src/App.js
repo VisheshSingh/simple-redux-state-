3249620c74a0c5e638f799eef68516e779568627
singhv <vishesh.singh@biworldwide.com>
2019-06-23 17:46:41 -0500

    dispatch action and modify state

--- src/App.js
+++ src/App.js
@@ -13,19 +13,31 @@ function App() {
       <h1 className="center purple-text darken-3">Redux</h1>
       <div className="container">
         <h3>Counter: {counter}</h3>
-        <button
-          className="btn purple darken-2"
-          onClick={() => dispatch(increment(2))}
-        >
-          +
-        </button>{' '}
-        &nbsp;
-        <button
-          className="btn purple lighten-2"
-          onClick={() => dispatch(decrement(1))}
-        >
-          -
-        </button>
+        <div className="section">
+          <button
+            className="btn purple darken-2"
+            onClick={() => dispatch(increment(2))}
+          >
+            +
+          </button>{' '}
+          &nbsp;
+          <button
+            className="btn purple lighten-2"
+            onClick={() => dispatch(decrement(1))}
+          >
+            -
+          </button>
+        </div>
+        <div className="divider" />
+        <div className="section">
+          <button
+            className="btn purple"
+            onClick={() => dispatch({ type: 'SIGN IN' })}
+          >
+            Log In
+          </button>
+          {isLoggedIn ? <p>Data only visible to authenticated users!</p> : ''}
+        </div>
       </div>
     </div>
   );
